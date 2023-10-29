e39c04f8913de24c51e2656c7d6c3cf9cefc519e
nadia-elm <elmefeddelnadia@gmail.com>
2023-10-29 14:35:27 +0400

    commit

--- db.js
+++ db.js
@@ -1,20 +1,17 @@
 'use strict';
+
 /** Database setup for store. */
 const { Client } = require('pg');
+
 const DB_URI = process.env.DATABASE_URL || 'postgresql:///storedb';
 
 let db = new Client({
   connectionString: DB_URI,
+  ssl:
+    process.env.NODE_ENV === 'production'
+      ? { rejectUnauthorized: false }
+      : false,
 });
-
-// let DB_URI = process.env.DATABASE_URL;
-
-// const client = new Client({
-//   connectionString: connectionString,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// });
 
 db.connect();
 
