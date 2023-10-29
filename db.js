192867289a11a547958392c4be03e6cc8d551c61
nadia-elm <elmefeddelnadia@gmail.com>
2023-10-29 13:30:09 +0400

    added db

--- db.js
+++ db.js
@@ -3,13 +3,13 @@
 const { Client } = require('pg');
 // const { getDatabaseUri } = require('./config');
 
-let DB_URI;
+let DB_URI = process.env.DATABASE_URL || 'postgresql:///storedb';
 
-if (process.env.NODE_ENV === 'test') {
-  DB_URI = 'postgresql:///storedb_test';
-} else {
-  DB_URI = 'postgresql:///storedb';
-}
+// if (process.env.NODE_ENV === 'test') {
+//   DB_URI = 'postgresql:///storedb_test';
+// } else {
+//   DB_URI = 'postgresql:///storedb';
+// }
 
 let db = new Client({
   connectionString: DB_URI,
